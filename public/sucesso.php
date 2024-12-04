0e77f6f17869736647d7d5702105a02b394b05a1
Junior36912 <juniojackson369@gmail.com>
2024-12-04 15:20:17 -0300

    first commit

---
+++ public/sucesso.php
@@ -0,0 +1,4 @@
+<h1>Sucesso! </h1>
+<p>Filme: <?php echo isset($_POST['filme']) ? $_POST['filme'] : 'Não informado'; ?> </p>
+<p>Lançamento: <?php echo isset($_POST['ano']) ? $_POST['ano'] : 'Não informado'; ?> </p>
+<p>Nota: <?php echo isset($_POST['nota']) ? $_POST['nota'] : 'Não informado'; ?> </p>
