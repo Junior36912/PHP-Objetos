209999a053f274dfa32e4695173d0cbce1d62815
Junior36912 <juniojackson369@gmail.com>
2024-12-04 18:51:53 -0300

    PHP - Objetos // Adição de conteudo e de atividade do Módulo II do curso de PHP e Objetos

--- src/Modelo/Filme.php
+++ src/Modelo/Filme.php
@@ -1,8 +1,46 @@
 <?php
 
 class Filme {
-    public string $nome;
-    public int $anoLancamento;
-    public string $genero;
-    public float $nota;
+    private string $nome = 'Nome padrão';
+    private int $anoLancamento = 2023;
+    private string $genero = 'ação';
+    private array $notas = [];
+
+
+    public function avalia(float $nota): void {
+        $this->notas[] = $nota;
+    }
+
+    public function media(): float {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+
+        return $somaNotas / $quantidadeNotas;
+    }
+
+    
+    public function GetNome() {
+        return $this->nome;
+    }
+
+    public function SetNome($nomeNovo) {
+        $this->nome = $nomeNovo;
+    }
+
+    public function GetAnoLancamento(): int {
+        return $this->anoLancamento;
+    }
+
+    public function SetAnoLancamento(int $anoLancamentoNovo) {
+        $this->anoLancamento = $anoLancamentoNovo;
+    }
+
+    public function GetGenero() {
+        return $this->genero;
+    }
+
+    public function SetGenero($generoNovo) {
+        $this->genero = $generoNovo;
+    }
 }
+
